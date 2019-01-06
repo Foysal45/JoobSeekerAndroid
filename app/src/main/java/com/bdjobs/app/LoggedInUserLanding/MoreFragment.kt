39e0c58a434939b1b58981fa6fd758ed4996c772
MD Rubayet Hassan <rubayet.evan@gmail.com>
2019-01-06 12:43:22 +0600

    More fragment Funtionality Added Applied job icon problem fixed

--- app/src/main/java/com/bdjobs/app/LoggedInUserLanding/MoreFragment.kt
+++ app/src/main/java/com/bdjobs/app/LoggedInUserLanding/MoreFragment.kt
@@ -12,6 +12,7 @@ import com.bdjobs.app.API.ModelClasses.MoreHorizontalData
 import com.bdjobs.app.Employers.EmployersBaseActivity
 import com.bdjobs.app.Jobs.JobBaseActivity
 import com.bdjobs.app.R
+import com.bdjobs.app.Utilities.openUrlInBrowser
 import kotlinx.android.synthetic.main.fragment_more_layout.*
 import org.jetbrains.anko.startActivity
 
@@ -19,6 +20,7 @@ class MoreFragment : Fragment() {
 
     private var horizontalAdapter: HorizontalAdapter? = null
     private var horizontaList: ArrayList<MoreHorizontalData> = ArrayList()
+    lateinit var homeCommunicator: HomeCommunicator
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_more_layout, container, false)!!
@@ -26,6 +28,7 @@ class MoreFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        homeCommunicator = activity as HomeCommunicator
         initializeViews()
         clearAddPopulateData()
         onclick()
@@ -33,6 +36,45 @@ class MoreFragment : Fragment() {
     private fun onclick() {
         employerList_MBTN.setOnClickListener {
             startActivity<EmployersBaseActivity>("from" to "employer")
+        }
+        generalSearch_MBTN.setOnClickListener {
+            startActivity<JobBaseActivity>("keyword" to "")
+        }
+        appGuides_MBTN.setOnClickListener {
+            activity.openUrlInBrowser("http://bdjobs.com/apps/ios/index.html")
+        }
+        rateUs_MBTN.setOnClickListener {
+            activity.openUrlInBrowser("https://play.google.com/store/apps/details?id=com.bdjobs.app")
+        }
+        feedback_MBTN.setOnClickListener {
+            activity.openUrlInBrowser("https://jobs.bdjobs.com/feedback.asp")
+        }
+        privacypolicy_MBTN.setOnClickListener {
+            activity.openUrlInBrowser("https://bdjobs.com/policy/Privacy_policy.asp")
+        }
+        terms_MBTN.setOnClickListener {
+            activity.openUrlInBrowser("https://www.bdjobs.com/tos.asp")
+        }
+        new_job_MBTN.setOnClickListener {
+            startActivity<JobBaseActivity>("postedWithin" to "1")
+        }
+        deadline_MBTN.setOnClickListener {
+            startActivity<JobBaseActivity>("deadline" to "2")
+        }
+        part_time_MBTN.setOnClickListener {
+            startActivity<JobBaseActivity>("jobNature" to "PartTime")
+        }
+        contractual_MBTN.setOnClickListener {
+            startActivity<JobBaseActivity>("jobNature" to "Contract")
+        }
+        government_MBTN.setOnClickListener {
+            startActivity<JobBaseActivity>("organization" to "1")
+        }
+        overseas_MBTN.setOnClickListener {
+            startActivity<JobBaseActivity>("location" to "Country")
+        }
+        interviewinvitation_MBTN.setOnClickListener {
+            homeCommunicator.goToInterviewInvitation("homePage")
         }
     }
     private fun clearAddPopulateData() {
