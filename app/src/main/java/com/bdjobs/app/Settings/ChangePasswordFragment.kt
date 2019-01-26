8300729b35b6e6c207715f11a42716e86ba31593
Firoz Hasan <diganto007@gmail.com>
2019-01-26 18:43:29 +0600

    change pass validation

--- app/src/main/java/com/bdjobs/app/Settings/ChangePasswordFragment.kt
+++ app/src/main/java/com/bdjobs/app/Settings/ChangePasswordFragment.kt
@@ -7,17 +7,25 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import com.bdjobs.app.API.ApiServiceMyBdjobs
 import com.bdjobs.app.API.ModelClasses.ChangePassword
-
 import com.bdjobs.app.R
 import com.bdjobs.app.SessionManger.BdjobsUserSession
+import com.bdjobs.app.Utilities.easyOnTextChangedListener
 import com.bdjobs.app.Utilities.error
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.fragment_change_password.*
 import org.jetbrains.anko.toast
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.regex.Pattern
+
+
+
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,9 +36,11 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  *
  */
+
 class ChangePasswordFragment : Fragment() {
     lateinit var bdjobsUserSession: BdjobsUserSession
     private lateinit var communicator: SettingsCommunicator
+    lateinit var symbol: String
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -45,6 +55,17 @@ class ChangePasswordFragment : Fragment() {
         communicator = activity as SettingsCommunicator
         tv_username.text = bdjobsUserSession.userName
         onClick()
+        et_old_pass.easyOnTextChangedListener { charSequence ->
+            validateoldPassword()
+        }
+        et_new_pass.easyOnTextChangedListener {charSequence ->
+            validateNewPassword()
+
+        }
+        et_confirm_pass.easyOnTextChangedListener {charSequence ->
+            validateConfirmPassword()
+
+        }
     }
 
     private fun onClick() {
@@ -88,6 +109,95 @@ class ChangePasswordFragment : Fragment() {
             communicator.backButtonPressed()
         }
     }
+
+    private fun validateoldPassword(): Boolean {
+        val pass = et_old_pass.getText().toString()
+        if (et_old_pass.getText().toString().trim({ it <= ' ' }).isEmpty()) {
+            return enableValidation(oldpassword_TIL,et_old_pass, getString(com.bdjobs.app.R.string.err_msg_password_not_empty) )
+        }
+
+  /*      else if (checkStringHasSymbol(pass)) {
+            return enableValidation(oldpassword_TIL,et_old_pass,"Password can not contain $symbol" )
+
+        } else if (pass.trim({ it <= ' ' }).length < 8 || pass.trim({ it <= ' ' }).length > 12) {
+            return enableValidation(oldpassword_TIL,et_old_pass, getString(com.bdjobs.app.R.string.err_msg_password_limit) )
+
+        }*/
+
+        else {
+            oldpassword_TIL.setErrorEnabled(false)
+        }
+
+        return true
+    }
+    private fun validateNewPassword(): Boolean {
+        val pass = et_new_pass.getText().toString()
+        if (et_new_pass.getText().toString().trim({ it <= ' ' }).isEmpty()) {
+            return enableValidation(newpassword_TIL,et_new_pass, getString(com.bdjobs.app.R.string.err_msg_password_not_empty) )
+
+        } else if (checkStringHasSymbol(pass)) {
+            return enableValidation(newpassword_TIL,et_new_pass, getString(com.bdjobs.app.R.string.err_msg_password_use))
+
+        } else if (pass.trim({ it <= ' ' }).length < 8 || pass.trim({ it <= ' ' }).length > 12) {
+            return enableValidation(newpassword_TIL,et_new_pass, getString(com.bdjobs.app.R.string.err_msg_password_limit))
+        } else {
+            newpassword_TIL.setErrorEnabled(false)
+        }
+
+        return true
+    }
+    private fun validateConfirmPassword(): Boolean {
+        val pass = et_confirm_pass.getText().toString()
+        if (et_confirm_pass.getText().toString().trim({ it <= ' ' }).isEmpty()) {
+            return enableValidation(confirmpassword_TIL,et_confirm_pass, getString(com.bdjobs.app.R.string.err_msg_password_confirm_not_empty) )
+        }
+        else if (!pass.equals(et_new_pass.text.toString(),true)) {
+            return enableValidation(confirmpassword_TIL,et_confirm_pass, getString(com.bdjobs.app.R.string.err_msg_password_not_match) )
+        }
+     /*   else if (checkStringHasSymbol(pass)) {
+            return enableValidation(confirmpassword_TIL,et_confirm_pass, getString(com.bdjobs.app.R.string.err_msg_password_use) )
+        } else if (pass.trim({ it <= ' ' }).length < 8 || pass.trim({ it <= ' ' }).length > 12) {
+            return enableValidation(confirmpassword_TIL,et_confirm_pass, getString(com.bdjobs.app.R.string.err_msg_password_limit) )
+        }*/
+        else {
+            confirmpassword_TIL.setErrorEnabled(false)
+        }
+
+        return true
+    }
+
+    private fun enableValidation(til : TextInputLayout, tied : TextInputEditText, errorText : String) : Boolean
+    {
+        til.setErrorEnabled(true)
+        til.setError(errorText)
+        requestFocus(tied)
+        return false
+    }
+
+    private fun checkStringHasSymbol(s: String): Boolean {
+
+        val p = Pattern.compile("[:`!@#$%&*()_ +/=;|'\"<>?{}\\[\\]~-]")
+
+        var b = false
+        for (i in 0 until s.length) {
+            symbol = s[i].toString()
+            val m = p.matcher(symbol)
+            b = m.matches()
+            if (b) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun requestFocus(view: View) {
+        if (view.requestFocus()) {
+           // getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+            activity!!.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+
+        }
+    }
+
 
 
 }
