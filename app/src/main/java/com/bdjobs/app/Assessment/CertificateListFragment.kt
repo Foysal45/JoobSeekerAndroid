05f0dd6020bd99e74e14bbd34e1294a3402917e4
rakib10rr3 <rakib10rr3@gmail.com>
2020-01-25 18:21:07 +0600

    Assessment certificate list and result ui added

--- app/src/main/java/com/bdjobs/app/Assessment/CertificateListFragment.kt
+++ app/src/main/java/com/bdjobs/app/Assessment/CertificateListFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import com.bdjobs.app.R
+import kotlinx.android.synthetic.main.fragment_certificate_list.*
 
 /**
  * A simple [Fragment] subclass.
@@ -18,6 +20,13 @@ class CertificateListFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_certificate_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        result_btn?.setOnClickListener {
+            findNavController().navigate(R.id.action_viewPagerFragment_to_resultFragment)
+        }
     }
 
 
