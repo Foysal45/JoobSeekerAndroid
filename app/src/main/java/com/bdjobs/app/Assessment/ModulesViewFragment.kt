68b4b2f8da4554ba7e6eeec2d03b1152a4d74f5d
masalvin <salvin.bdjobs@gmail.com>
2020-02-04 17:08:08 +0600

    view module fragment partially implemented.

---
+++ app/src/main/java/com/bdjobs/app/Assessment/ModulesViewFragment.kt
@@ -0,0 +1,29 @@
+package com.bdjobs.app.Assessment
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+
+import com.bdjobs.app.R
+import kotlinx.android.synthetic.main.fragment_test_location.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ModulesViewFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_module_view, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+}
