c8905718a22fcf1c7e05b687d39cffa29df43bc6
suriya <taniacse1@gmail.com>
2020-06-09 20:27:33 +0600

    interview invitationlist done with functionality

--- app/src/main/java/com/bdjobs/app/videoInterview/ui/interview_list/BindingAdapaterInterviewList.kt
+++ app/src/main/java/com/bdjobs/app/videoInterview/ui/interview_list/BindingAdapaterInterviewList.kt
@@ -1,11 +1,13 @@
 package com.bdjobs.app.videoInterview.ui.interview_list
 
 import android.graphics.Color
+import android.media.Image
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
@@ -16,6 +18,8 @@ import com.bdjobs.app.assessment.models.ScheduleData
 
 import com.bdjobs.app.videoInterview.data.models.InterviewListData
 import com.facebook.shimmer.ShimmerFrameLayout
+import org.jetbrains.anko.backgroundColor
+import org.jetbrains.anko.textColor
 
 @BindingAdapter("InterviewList")
 fun bindInterviewListRecyclerView(recyclerView: RecyclerView, data: List<InterviewListData?>?) {
@@ -68,11 +72,11 @@ fun bindInterviewEmptyView(constraintLayout: ConstraintLayout, data: List<Interv
 }
 
 @BindingAdapter("interviewShimmer")
-fun bindInterviewListShimmer(shimmerFrameLayout: ShimmerFrameLayout, status : Status?){
+fun bindInterviewListShimmer(shimmerFrameLayout: ShimmerFrameLayout, status: Status?) {
 
     status?.let {
         when (status) {
-            Status.LOADING ->  {
+            Status.LOADING -> {
                 //Log.d("rakib", "loading")
                 shimmerFrameLayout.visibility = View.VISIBLE
                 shimmerFrameLayout.startShimmer()
@@ -90,17 +94,15 @@ fun bindInterviewListShimmer(shimmerFrameLayout: ShimmerFrameLayout, status : St
     }
 
 
-
-
 }
 
 
 @BindingAdapter("interviewCLVisibility")
-fun bindInterviewListShimmer(constraintLayout: ConstraintLayout, status : Status?){
+fun bindInterviewListShimmer(constraintLayout: ConstraintLayout, status: Status?) {
 
     status?.let {
         when (status) {
-            Status.LOADING ->  {
+            Status.LOADING -> {
                 //Log.d("rakib", "loading")
                 constraintLayout.visibility = View.GONE
 
@@ -118,60 +120,227 @@ fun bindInterviewListShimmer(constraintLayout: ConstraintLayout, status : Status
 }
 
 
-
 @BindingAdapter("interviewNoDataVisibility")
-fun bindNoData(linearLayout: LinearLayout, data: List<InterviewListData?>?){
+fun bindNoData(linearLayout: LinearLayout, data: List<InterviewListData?>?) {
 
     data?.let {
-       if (data.isEmpty()){
-           linearLayout.visibility = View.VISIBLE
-       } else
-           linearLayout.visibility = View.GONE
+        if (data.isEmpty()) {
+            linearLayout.visibility = View.VISIBLE
+        } else
+            linearLayout.visibility = View.GONE
     }
 }
 
 @BindingAdapter("inviteAndSubmit")
-fun bindInviteAndSubmitView(view: View, data:InterviewListData) {
-
-
-
+fun bindInviteAndSubmitView(view: View, data: InterviewListData) {
     when (view) {
         is ImageView -> {
-            if (data.invitationSubmitedDate.isNullOrEmpty()){
+            if (data.invitationSubmitedDate.isNullOrEmpty()) {
                 //Log.d("rakib", "empty view called $data")
                 view.background = ContextCompat.getDrawable(view.context, R.drawable.ic_calender_interview_invitation)
-            } else {
+            } else if (data.invitationSubmitedDate.isNotEmpty()) {
                 view.background = ContextCompat.getDrawable(view.context, R.drawable.ic_submitted)
             }
         }
 
 
-       is TextView -> {
-           if (data.invitationSubmitedDate.isNullOrEmpty()){
-               view.text = "InvitedOn: ${data.inviteDate}"
-               view.setTextColor(Color.parseColor("#393939"))
-           } else {
-               view.text = "SubmittedOn: ${data.invitationSubmitedDate}"
-               view.setTextColor(Color.parseColor("#388E3C"))
-           }
+        is TextView -> {
+            if (data.invitationSubmitedDate.isNullOrEmpty()) {
+                view.text = "InvitedOn: ${data.inviteDate}"
+                view.setTextColor(Color.parseColor("#393939"))
+            } else if (data.invitationSubmitedDate.isNotEmpty()) {
+                view.text = "SubmittedOn: ${data.invitationSubmitedDate}"
+                view.setTextColor(Color.parseColor("#388E3C"))
+            }
         }
 
-       /* Status.ERROR -> {
-            constraintLayout.visibility = View.VISIBLE
-        }*/
+        /* Status.ERROR -> {
+             constraintLayout.visibility = View.VISIBLE
+         }*/
     }
 }
 
 
-/*
-@BindingAdapter("inviteSubmitVisibility")
-fun bindNoData(view: View, date: String){
 
-    data?.let {
-       if (data.isEmpty()){
-           linearLayout.visibility = View.VISIBLE
-       } else
-           linearLayout.visibility = View.GONE
+
+@BindingAdapter("interviewItemBackground")
+fun bindInterviewItemBackground(view: View, data: InterviewListData) {
+    when (view) {
+        is ImageView -> {
+            if (data.invitationSubmitedDate.isNullOrEmpty()) {
+                //Log.d("rakib", "empty view called $data")
+                view.background = ContextCompat.getDrawable(view.context, R.drawable.ic_calender_interview_invitation)
+            } else if (data.invitationSubmitedDate.isNotEmpty()) {
+                view.background = ContextCompat.getDrawable(view.context, R.drawable.ic_submitted)
+            }
+        }
+
+
+        is TextView -> {
+            if (data.invitationSubmitedDate.isNullOrEmpty()) {
+                view.text = "InvitedOn: ${data.inviteDate}"
+                view.setTextColor(Color.parseColor("#393939"))
+            } else if (data.invitationSubmitedDate.isNotEmpty()) {
+                view.text = "SubmittedOn: ${data.invitationSubmitedDate}"
+                view.setTextColor(Color.parseColor("#388E3C"))
+            }
+        }
+
+        /* Status.ERROR -> {
+             constraintLayout.visibility = View.VISIBLE
+         }*/
     }
 }
-*/
+
+
+@BindingAdapter("seenVisibility")
+fun bindIconVisibility(view: View, invitationDate: String) {
+
+    invitationDate?.let {
+        if (invitationDate.isEmpty()) {
+            view.visibility = View.GONE
+        } else if (invitationDate.isNotEmpty())
+            view.visibility = View.VISIBLE
+    }
+}
+
+
+@BindingAdapter("onProcessSubmission")
+fun bindOnProcessSubmission(view: View, statusCode: String) {
+
+    Log.d("ksflsknfjdfndjnfj", "In  bindOnProcessSubmission ${statusCode}")
+    statusCode?.let {
+
+        when (statusCode) {
+            "4" -> {
+                when (view) {
+                    is ImageView -> {
+                        view.background = ContextCompat.getDrawable(view.context, R.drawable.ic_on_process)
+
+                    }
+                    is TextView -> {
+
+                        view.text = "On Process"
+                        view.setTextColor(Color.parseColor("#F57F17"))
+                    }
+
+
+                    is CardView -> {
+
+                        view.backgroundColor = Color.parseColor("#F8F6EA")
+                    }
+
+                    is ConstraintLayout ->{
+                        view.background = ContextCompat.getDrawable(view.context, R.drawable.ic_shape_yello)
+
+
+                    }
+                }
+
+
+            }
+            "5" -> {
+                when (view) {
+                    is ImageView -> {
+                        view.background = ContextCompat.getDrawable(view.context, R.drawable.ic_due_submission)
+
+                    }
+                    is TextView -> {
+
+                        view.text = "Deu Submisssion"
+                        view.setTextColor(Color.parseColor("#B71C1C"))
+                    }
+
+                    is CardView -> {
+
+                        view.backgroundColor = Color.parseColor("#F8F6EA")
+                    }
+
+                    is ConstraintLayout ->{
+                        view.background = ContextCompat.getDrawable(view.context, R.drawable.ic_shape_yello)
+
+
+                    }
+                }
+            }
+            "1" -> {
+                when (view) {
+                    is ImageView -> {
+                        view.visibility = View.INVISIBLE
+
+                    }
+                    is TextView -> {
+
+                        view.visibility = View.INVISIBLE
+                    }
+
+
+                    is CardView -> {
+
+                        view.backgroundColor = Color.parseColor("#FFFFFF")
+                    }
+
+                    is ConstraintLayout ->{
+                        view.background = ContextCompat.getDrawable(view.context, R.drawable.ic_shape_white)
+
+
+                    }
+                }
+            }
+            "2" -> {
+
+
+                Log.d("ksflsknfjdfndjnfj", "In  condition 2")
+                when (view) {
+                    is ImageView -> {
+                        view.visibility = View.INVISIBLE
+
+                    }
+                    is TextView -> {
+
+                        view.visibility = View.INVISIBLE
+                    }
+
+                    is CardView -> {
+
+                        view.backgroundColor = Color.parseColor("#F8F6EA")
+                    }
+
+                    is ConstraintLayout ->{
+                        view.background = ContextCompat.getDrawable(view.context, R.drawable.ic_shape_yello)
+
+
+                    }
+                }
+            }
+            "3" -> {
+                when (view) {
+                    is ImageView -> {
+                        view.visibility = View.INVISIBLE
+
+                    }
+                    is TextView -> {
+
+                        view.visibility = View.INVISIBLE
+                    }
+
+
+                    is CardView -> {
+
+                        view.backgroundColor = Color.parseColor("#F8F6EA")
+                    }
+
+                    is ConstraintLayout ->{
+                        view.background = ContextCompat.getDrawable(view.context, R.drawable.ic_shape_yello)
+
+
+                    }
+                }
+            }
+
+
+        }
+
+    }
+
+}
