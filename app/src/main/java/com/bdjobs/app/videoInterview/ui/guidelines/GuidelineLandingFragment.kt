c7c857b428f71fcc123c02a78ba4aee73b09be39
Rakibul Huda <rakib10rr3@gmail.com>
2020-06-14 20:30:55 +0600

    added english in guidelines

--- app/src/main/java/com/bdjobs/app/videoInterview/ui/guidelines/GuidelineLandingFragment.kt
+++ app/src/main/java/com/bdjobs/app/videoInterview/ui/guidelines/GuidelineLandingFragment.kt
@@ -1,10 +1,10 @@
 package com.bdjobs.app.videoInterview.ui.guidelines
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navGraphViewModels
@@ -14,8 +14,8 @@ import kotlinx.android.synthetic.main.fragment_guideline_landing.*
 
 class GuidelineLandingFragment : Fragment() {
 
-    private val guidelineVIewModel : GuidelineVIewModel by navGraphViewModels(R.id.guidelineLandingFragment)
-    lateinit var binding : FragmentGuidelineLandingBinding
+    private val guidelineVIewModel: GuidelineVIewModel by navGraphViewModels(R.id.guidelineLandingFragment)
+    lateinit var binding: FragmentGuidelineLandingBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -35,27 +35,38 @@ class GuidelineLandingFragment : Fragment() {
         }
 
         btn_start?.setOnClickListener {
-            findNavController().navigate(R.id.action_guidelineLandingFragment_to_guidelinesViewpagerFragment)
+
+            var language = if (guidelineVIewModel.viewInBangla.value!!) "bangla" else "english"
+
+            findNavController().navigate(GuidelineLandingFragmentDirections.actionGuidelineLandingFragmentToGuidelinesViewpagerFragment(language))
         }
 
         guidelineVIewModel.apply {
             viewInBangla.observe(viewLifecycleOwner, Observer { checked ->
-                if (checked){
+                if (checked) {
                     cb_view_in_english?.isChecked = false
                     cb_view_in_bangla?.isEnabled = false
                     cb_view_in_english?.isEnabled = true
-                } else{
+                } else {
                     cb_view_in_bangla?.isEnabled = true
                 }
+
+                tv_guideline_heading.text = "ভিডিও ইন্টারভিউ এর গাইডলাইনগুলো"
+                tv_guideline_body.text = "ভিডিও রেকর্ডিং শুরু করার আগে প্রয়োজনীয় ডিভাইসগুলো এবং নির্দেশনাগুলো সম্পর্কে প্রথমেই জেনে নিন"
+                btn_start.text = "শুরু করুন"
             })
-            viewInEnglish.observe(viewLifecycleOwner, Observer {checked ->
-                if (checked){
+            viewInEnglish.observe(viewLifecycleOwner, Observer { checked ->
+                if (checked) {
                     cb_view_in_bangla?.isChecked = false
                     cb_view_in_english?.isEnabled = false
                     cb_view_in_bangla?.isEnabled = true
-                } else{
+                } else {
                     cb_view_in_bangla?.isEnabled = true
                 }
+
+                tv_guideline_heading.text = "Guidelines for Video Interview"
+                tv_guideline_body.text = "Learn about the required devices and Interview instructions before you start recording video."
+                btn_start.text = "Start"
             })
         }
 
