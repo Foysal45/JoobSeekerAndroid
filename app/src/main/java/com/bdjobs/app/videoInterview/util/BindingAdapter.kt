7ef59107011419f45215545954a2b59103d2d921
Rakibul Huda <rakib10rr3@gmail.com>
2020-06-25 14:26:42 +0600

    warning text changed

--- app/src/main/java/com/bdjobs/app/videoInterview/util/BindingAdapter.kt
+++ app/src/main/java/com/bdjobs/app/videoInterview/util/BindingAdapter.kt
@@ -329,15 +329,6 @@ fun bindInterviewProcessStatus(view: View, videoInterviewData: VideoInterviewLis
 
 }
 
-@BindingAdapter("warningText")
-fun bindWarningText(textView: TextView, totalAnswers: String?) {
-
-//    totalAnswers?.let {
-//        textView.text = if (it.toInt() > 0) textView.context.getText(R.string.video_not_interested_some_answered) else textView.context.getText(R.string.video_not_interested_none_answered)
-//    }
-    textView.context.getText(R.string.video_not_interested_generic_text)
-}
-
 @BindingAdapter("visibilityWhenTimerShowing", "visibilityWhenSubmitted")
 fun bindOneHourInfoVisibility(view: ConstraintLayout, remainingTime: String?, userSubmittedAnswer: String?) {
     userSubmittedAnswer?.let {
