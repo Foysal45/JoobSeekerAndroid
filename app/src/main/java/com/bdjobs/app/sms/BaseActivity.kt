b183ac0f81d096e5ed6d8a2db9463cfc88774be5
Rakibul Huda <rakib10rr3@gmail.com>
2020-07-25 12:21:15 +0600

    navigation logic fix

--- app/src/main/java/com/bdjobs/app/sms/BaseActivity.kt
+++ app/src/main/java/com/bdjobs/app/sms/BaseActivity.kt
@@ -2,16 +2,10 @@ package com.bdjobs.app.sms
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.bdjobs.app.R
-import com.bdjobs.app.Utilities.Constants
-import com.bdjobs.app.Utilities.equalIgnoreCase
-import com.bdjobs.app.Utilities.hide
-import com.bdjobs.app.Utilities.show
 import kotlinx.android.synthetic.main.activity_base.*
 
 class BaseActivity : AppCompatActivity() {
@@ -21,14 +15,29 @@ class BaseActivity : AppCompatActivity() {
 
         val from = intent.getStringExtra("from")
 
+        val navHostFragment = smsNavHostFragment as NavHostFragment
+        val inflater = navHostFragment.navController.navInflater
+        val graph = inflater.inflate(R.navigation.sms_nav_graph)
+
+        when (from) {
+            "employer", "favourite" -> {
+                graph.startDestination = R.id.settingsFragment
+            }
+            else -> {
+                graph.startDestination = R.id.smsHomeFragment
+            }
+        }
+
+        navHostFragment.navController.graph = graph
+
         val appBarConfiguration = AppBarConfiguration.Builder()
                 .setFallbackOnNavigateUpListener { onNavigateUp() }
                 .build()
-        tool_bar?.setupWithNavController(this.findNavController(R.id.smsNavHostFragment), appBarConfiguration)
+        tool_bar?.setupWithNavController(navHostFragment.navController, appBarConfiguration)
     }
 
     override fun onNavigateUp(): Boolean {
-        super.onNavigateUp()
+        super.finish()
         return true
     }
 }
