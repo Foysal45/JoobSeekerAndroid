d6ed0084fbf83575e04a8652364963e94f5fa165
Rakibul Huda <rakib10rr3@gmail.com>
2020-09-06 10:42:52 +0600

    added live interview list page

---
+++ app/src/main/java/com/bdjobs/app/liveInterview/interview_details/LiveInterviewDetailsFragment.kt
@@ -0,0 +1,19 @@
+package com.bdjobs.app.liveInterview.interview_details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bdjobs.app.R
+
+class LiveInterviewDetailsFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_live_interview_details, container, false)
+    }
+
+}
