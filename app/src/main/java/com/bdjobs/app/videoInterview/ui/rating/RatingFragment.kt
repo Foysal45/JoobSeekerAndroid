772fa9d939dbff802742c6e6742539515363a2d8
Rakib <rakib10rr3@gmail.com>
2020-11-10 13:05:28 +0600

    added video interview rating

--- app/src/main/java/com/bdjobs/app/videoInterview/ui/rating/RatingFragment.kt
+++ app/src/main/java/com/bdjobs/app/videoInterview/ui/rating/RatingFragment.kt
@@ -6,10 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.viewModels
 import com.bdjobs.app.R
 import com.bdjobs.app.databinding.FragmentRatingBinding
 import com.bdjobs.app.videoInterview.data.repository.VideoInterviewRepository
+import kotlinx.android.synthetic.main.fragment_rating.*
+import org.jetbrains.anko.sdk27.coroutines.onRatingBarChange
+import org.jetbrains.anko.support.v4.toast
+import timber.log.Timber
 
 class RatingFragment : Fragment() {
 
@@ -30,4 +35,16 @@ class RatingFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN or WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN)
+
+        rating?.onRatingBarChange { ratingBar, rating, fromUser ->
+            toast("${rating.toInt()}")
+            ratingViewModel.apply {
+                this.rating.value = rating.toInt()
+                onRatingChanged()
+            }
+        }
+    }
 }
