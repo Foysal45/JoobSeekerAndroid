185f8b03c69cd1ffec7432f2b4f707c5fc2182b9
Soumik Bhattacharjee <soumikcse07@gmail.com>
2021-04-08 18:20:06 +0600

    client ad added in job list and details page

---
+++ app/src/main/java/com/bdjobs/app/API/ModelClasses/ClientAdModel.kt
@@ -0,0 +1,38 @@
+package com.bdjobs.app.API.ModelClasses
+import androidx.annotation.Keep
+import com.squareup.moshi.JsonClass
+
+import com.squareup.moshi.Json
+
+
+
+//
+// Created by Soumik on 4/8/2021.
+// Copyright (c) 2021 Soumik.  All rights reserved.
+//@JsonClass(generateAdapter = true)
+
+@Keep
+data class ClientAdModel(
+    @field:Json(name = "data")
+    val `data`: List<DataClientAd>,
+    @field:Json(name = "error")
+    val error: String,
+    @field:Json(name = "Success")
+    val success: Int
+)
+
+@JsonClass(generateAdapter = true)
+data class DataClientAd(
+    @field:Json(name = "adurl")
+    val adurl: String,
+    @field:Json(name = "height")
+    val height: Int,
+    @field:Json(name = "imageurl")
+    val imageurl: String,
+    @field:Json(name = "position")
+    val position: String,
+    @field:Json(name = "unit")
+    val unit: String,
+    @field:Json(name = "width")
+    val width: Int
+)
