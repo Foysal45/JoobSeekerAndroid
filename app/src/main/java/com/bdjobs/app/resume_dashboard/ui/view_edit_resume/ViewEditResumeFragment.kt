283a12015f4f578446c7656ac01bd77788d996cb
Soumik Bhattacharjee <soumikcse07@gmail.com>
2021-06-22 14:13:26 +0600

    details API implementation ongoing

--- app/src/main/java/com/bdjobs/app/resume_dashboard/ui/view_edit_resume/ViewEditResumeFragment.kt
+++ app/src/main/java/com/bdjobs/app/resume_dashboard/ui/view_edit_resume/ViewEditResumeFragment.kt
@@ -7,10 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.navGraphViewModels
 import com.bdjobs.app.R
 import com.bdjobs.app.databinding.ViewEditResumeFragmentBinding
+import com.bdjobs.app.resume_dashboard.data.models.DataMRD
 import com.bdjobs.app.resume_dashboard.data.repositories.ResumeDashboardRepository
+import kotlinx.android.synthetic.main.layout_bdjobs_resume_steps.*
 
 class ViewEditResumeFragment : Fragment() {
 
@@ -34,6 +37,49 @@ class ViewEditResumeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+
+        setUpObservers()
+    }
+
+    private fun setUpObservers() {
+        viewEditResumeViewModel.apply {
+
+            detailResumeStat.observe(viewLifecycleOwner, Observer {
+                bdJobsResumeStepStatus(it)
+            })
+        }
+    }
+
+    private fun bdJobsResumeStepStatus(it: DataMRD) {
+        if (it.personalDetailsInfo == 1) tv_personal_details.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_check_sign_resume, 0, R.drawable.ic_back_arrow_resume, 0)
+        else tv_personal_details.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_cross_sign_resume, 0,  R.drawable.ic_back_arrow_resume, 0)
+
+        if (it.academicInfo == 1) tv_academic_qualification.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_check_sign_resume, 0, R.drawable.ic_back_arrow_resume, 0)
+        else tv_academic_qualification.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_cross_sign_resume, 0,  R.drawable.ic_back_arrow_resume, 0)
+
+        if (it.trainingInfo == 1) tv_training.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_check_sign_resume, 0, R.drawable.ic_back_arrow_resume, 0)
+        else tv_training.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_cross_sign_resume, 0,  R.drawable.ic_back_arrow_resume, 0)
+
+        if (it.proQualificationInfo == 1) tv_professional_qualification.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_check_sign_resume, 0, R.drawable.ic_back_arrow_resume, 0)
+        else tv_professional_qualification.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_cross_sign_resume, 0,  R.drawable.ic_back_arrow_resume, 0)
+
+        if (it.experienceInfo == 1) tv_experience.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_check_sign_resume, 0, R.drawable.ic_back_arrow_resume, 0)
+        else tv_experience.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_cross_sign_resume, 0,  R.drawable.ic_back_arrow_resume, 0)
+
+        if (it.specializationInfo == 1) tv_specialization.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_check_sign_resume, 0, R.drawable.ic_back_arrow_resume, 0)
+        else tv_specialization.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_cross_sign_resume, 0,  R.drawable.ic_back_arrow_resume, 0)
+
+        if (it.referenceInfo == 1) tv_reference.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_check_sign_resume, 0, R.drawable.ic_back_arrow_resume, 0)
+        else tv_reference.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_cross_sign_resume, 0,  R.drawable.ic_back_arrow_resume, 0)
+
+        if (it.photographInfo == 1) tv_photograph.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_check_sign_resume, 0, R.drawable.ic_back_arrow_resume, 0)
+        else tv_photograph.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_cross_sign_resume, 0,  R.drawable.ic_back_arrow_resume, 0)
+    }
+
+    private fun initViews() {
+        binding.tvChangeVisibility.paint.isUnderlineText = true
     }
 
 }
--- app/src/main/java/com/bdjobs/app/resume_dashboard/ui/view_edit_resume/ViewEditResumeViewModel.kt
+++ app/src/main/java/com/bdjobs/app/resume_dashboard/ui/view_edit_resume/ViewEditResumeViewModel.kt
@@ -1,5 +1,6 @@
 package com.bdjobs.app.resume_dashboard.ui.view_edit_resume
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,6 +9,7 @@ import com.bdjobs.app.resume_dashboard.data.models.DataMRD
 import com.bdjobs.app.resume_dashboard.data.repositories.ResumeDashboardRepository
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.text.SimpleDateFormat
 
 class ViewEditResumeViewModel(private val repository: ResumeDashboardRepository) : ViewModel() {
 
@@ -24,18 +26,29 @@ class ViewEditResumeViewModel(private val repository: ResumeDashboardRepository)
     var isLoading = MutableLiveData<Boolean>()
 
     private var _detailsResumeStat = MutableLiveData<DataMRD>()
-    val detailResumeStat : LiveData<DataMRD> get() = _detailsResumeStat
+    val detailResumeStat: LiveData<DataMRD> get() = _detailsResumeStat
 
-    var bdJobsStatusPercentage = MutableLiveData<String>().apply { value = "" }
+    var bdJobsResumeStatusPercentage = MutableLiveData<Int>().apply { value=0 }
+    var bdJobsResumeLastUpdate = MutableLiveData<String>().apply { value = "" }
+
+
+    var videoResumeStatusPercentage = MutableLiveData<Int>().apply { value=0 }
+    var videoResumeLastUpdate = MutableLiveData<String>().apply { value = "" }
+    var isVideoResumeShowingToEmp = MutableLiveData<Boolean>()
+
+    var isPersonalizedResumeAvailable = MutableLiveData<Boolean>()
+    var personalizedResumeLastUpload = MutableLiveData<String>().apply { value = "" }
 
     init {
         showBdJobsResumeSteps.value = false
         showVideoResumeSteps.value = false
 
+        resumePrivacyStatus()
         manageResumeDetailsStat()
     }
 
 
+    @SuppressLint("SimpleDateFormat")
     private fun manageResumeDetailsStat() {
         isLoading.value = true
 
@@ -48,21 +61,84 @@ class ViewEditResumeViewModel(private val repository: ResumeDashboardRepository)
                     val data = response.data!![0]
                     _detailsResumeStat.value = data
 
-                    bdJobsStatusPercentage.value = data.bdjobsStatusPercentage
+                    bdJobsResumeStatusPercentage.value = data.bdjobsStatusPercentage?.toInt()
+                    bdJobsResumeLastUpdate.value = formatDate(data.bdjobsLastUpdateDate)
+
+                    videoResumeStatusPercentage.value = data.videoStatusPercentage?.toInt()
+                    videoResumeLastUpdate.value = formatDateVP(data.videoLastUpdateDate)
+                    isVideoResumeShowingToEmp.value = data.videoResumeVisibility=="1"
+
+                    isPersonalizedResumeAvailable.value = data.personalizefileName!=""
+                    if (data.personalizeLastUpdateDate!="") personalizedResumeLastUpload.value = formatDateVP(data.personalizeLastUpdateDate)
 
                 } else {
                     Timber.e("Invalid response")
                 }
 
-            } catch (e:Exception) {
+            } catch (e: Exception) {
                 Timber.e("Error while fetching details resume stat")
                 isLoading.value = false
             }
         }
     }
 
+    private fun resumePrivacyStatus() {
+        isLoading.value = true
+
+        viewModelScope.launch {
+            try {
+
+                val response = repository.resumePrivacyStatus()
+
+//                isLoading.value = false
+
+                if (response.statuscode == "0" && response.message == "Success") {
+                    val data = response.data!![0]
+
+                    // 1 -> public 2-> private 3-> limited
+
+                    _resumeVisibility.value = data?.resumeVisibilityType!!
+
+                } else {
+//                    isLoading.value = false
+                }
+            } catch (e: Exception) {
+//                isLoading.value = false
+            }
+        }
+
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun formatDate(lastUpdate: String?):String {
+        var lastUpdate1 = lastUpdate
+        var formatter = SimpleDateFormat("M/dd/yyyy")
+        val date = formatter.parse(lastUpdate1!!)
+        formatter = SimpleDateFormat("dd MMM yyyy")
+        lastUpdate1 = formatter.format(date!!)
+
+        Timber.d("Last updated at: $lastUpdate1")
+
+        return lastUpdate1
+
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun formatDateVP(lastUpdate: String?):String {
+        var lastUpdate1 = lastUpdate
+        var formatter = SimpleDateFormat("M/dd/yyyy HH:mm:ss a")
+        val date = formatter.parse(lastUpdate1!!)
+        formatter = SimpleDateFormat("dd MMM yyyy")
+        lastUpdate1 = formatter.format(date!!)
+
+        Timber.d("Last updated at: $lastUpdate1")
+
+        return lastUpdate1
+
+    }
+
     fun onBdJobsResumeStepClicked() {
-        showBdJobsResumeSteps.value = showBdJobsResumeSteps.value==false
+        showBdJobsResumeSteps.value = showBdJobsResumeSteps.value == false
     }
 
     fun onVideoResumeStepsClicked() {
