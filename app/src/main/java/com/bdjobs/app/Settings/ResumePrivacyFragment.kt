aff2fc4c1b27198df34290877bb17382c4456d40
Soumik Bhattacharjee <soumikcse07@gmail.com>
2021-06-26 17:51:29 +0600

    resume privacy UI done

--- app/src/main/java/com/bdjobs/app/Settings/ResumePrivacyFragment.kt
+++ app/src/main/java/com/bdjobs/app/Settings/ResumePrivacyFragment.kt
@@ -6,17 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bdjobs.app.R
+import com.bdjobs.app.databinding.FragmentResumePrivacyBinding
 
 
 class ResumePrivacyFragment : Fragment() {
 
+    private lateinit var binding : FragmentResumePrivacyBinding
+    private lateinit var communicator: SettingsCommunicator
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_resume_privacy, container, false)
+    ): View {
+        binding = FragmentResumePrivacyBinding.inflate(inflater)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        communicator = activity as SettingsCommunicator
+        binding.backIV.setOnClickListener {
+            communicator.backButtonPressed()
+        }
     }
 
 
