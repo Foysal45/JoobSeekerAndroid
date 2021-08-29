ecf0223a5d818da9ca0a262a6b1aad788fc397b8
Soumik Bhattacharjee <soumikcse07@gmail.com>
2021-08-29 18:21:52 +0600

    question and guideline corrected

--- app/src/main/java/com/bdjobs/app/videoResume/data/repository/VideoResumeRepository.kt
+++ app/src/main/java/com/bdjobs/app/videoResume/data/repository/VideoResumeRepository.kt
@@ -33,7 +33,7 @@ class VideoResumeRepository(private val application: Application) {
         return listOf(
             Question("১","নিজের সম্পর্কে কিছু বলুন","০০:৩০", isNew = false, isSubmitted = false),
             Question("২","আপনার শিক্ষাগত যোগ্যতা বা ট্রেনিং সম্পর্কে বলুন (যদি থাকে)","০০:৩০", isNew = false, isSubmitted = false),
-            Question("৩","আপনার দক্ষতা সম্পর্কে বলুন (চাকরিতে/শিক্ষাক্ষেত্রে)","০০:৩০", isNew = false, isSubmitted = false),
+            Question("৩","আপনার চাকরির অভিজ্ঞতা(যদি থাকে) অথবা দক্ষতা সম্পর্কে বলুন।","০০:৩০", isNew = false, isSubmitted = false),
             Question("৪","আপনার ভাল দিক এবং দুর্বল দিকগুলো সম্পর্কে বলুন ?","০০:৩০", isNew = false, isSubmitted = false),
             Question("৫","আপনার কর্মজীবনের লক্ষ্য সম্পর্কে বলুন ?","০০:৩০", isNew = false, isSubmitted = false),
             Question("৬","আপনার অর্জন এবং এক্সট্রা কারিকুলার এক্টিভিটিস সম্পর্কে বলুন (যদি থাকে)","০০:৩০", isNew = false, isSubmitted = false),
