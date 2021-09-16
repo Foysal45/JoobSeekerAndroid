c25fd107a75130ce56eed844ef67dcfc569c2c00
Ismail Hossain Sepon <ismailhossainsepon@gmail.com>
2021-09-16 20:26:23 +0600

    add Camera for lower device

--- app/src/main/java/com/bdjobs/app/videoResume/ui/record/RecordVideoResumeFragment.kt
+++ app/src/main/java/com/bdjobs/app/videoResume/ui/record/RecordVideoResumeFragment.kt
@@ -3,15 +3,24 @@ package com.bdjobs.app.videoResume.ui.record
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.AlertDialog
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.os.StrictMode
+import android.provider.MediaStore
 import android.text.Html
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
@@ -44,6 +53,11 @@ import java.util.concurrent.Executors
 class RecordVideoResumeFragment : Fragment() {
     lateinit var snackbar: Snackbar
     lateinit var videoFile: File
+    lateinit var _videoFile_for_lower: File
+    private val REQUEST_ID_READ_WRITE_PERMISSION = 99
+    private val REQUEST_ID_IMAGE_CAPTURE = 100
+    private val REQUEST_ID_VIDEO_CAPTURE = 101
+    lateinit var   videoView : VideoView
     var sdk = 0
 
     private val videoResumeQuestionsViewModel: VideoResumeQuestionsViewModel by navGraphViewModels(R.id.videoResumeQuestionsFragment)
@@ -128,7 +142,14 @@ class RecordVideoResumeFragment : Fragment() {
         recordVideoResumeViewModel.apply {
             onVideoRecordingStartedEvent.observe(viewLifecycleOwner, EventObserver {
                 updateUI(it)
-                captureVideo()
+                if (sdk < 23){
+
+                    askPermissionAndCaptureVideo()
+
+                }else{
+
+                    captureVideo()
+                }
             })
 
             progressPercentage.observe(viewLifecycleOwner, {
@@ -223,7 +244,6 @@ class RecordVideoResumeFragment : Fragment() {
                         recordVideoResumeViewModel.videoResumeManagerData.value?.file = newFile
                         recordVideoResumeViewModel.uploadSingleVideoToServer(recordVideoResumeViewModel.videoResumeManagerData.value)
                     showSnackbar()
-
                 }
 
 
@@ -330,4 +350,112 @@ class RecordVideoResumeFragment : Fragment() {
     override fun onStop() {
         super.onStop()
     }
+
+
+
+    private fun askPermissionAndCaptureVideo() {
+        // for permission to read/write data on the device.
+        if (Build.VERSION.SDK_INT >= 23) {
+
+            // Check if we have read/write permission
+            val readPermission = ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+            val writePermission = ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            if (writePermission != PackageManager.PERMISSION_GRANTED ||
+                readPermission != PackageManager.PERMISSION_GRANTED
+            ) {
+                // If don't have permission so prompt the user.
+                requestPermissions(
+                    arrayOf(
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ), REQUEST_ID_READ_WRITE_PERMISSION
+                )
+                return
+            }
+        }
+        recordLowerVideo()
+    }
+
+    private fun recordLowerVideo() {
+        try {
+            // Create an implicit intent, for video capture.
+            val intent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
+            val dir = File(requireContext().getExternalFilesDir(null)!!.absoluteFile, "video_resume")
+            dir.mkdirs()
+            val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+            val newFile = File(dir.path + File.separator + "bdjobs_${recordVideoResumeViewModel.videoResumeManagerData.value?.questionId}_$timeStamp.mp4")
+
+            // Specify where to save video files.
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, newFile)
+            intent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 30)
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+
+            val builder = StrictMode.VmPolicy.Builder()
+            StrictMode.setVmPolicy(builder.build())
+            startActivityForResult(intent, REQUEST_ID_VIDEO_CAPTURE) // (**)
+            _videoFile_for_lower = newFile
+
+        } catch (e: Exception) {
+
+            Timber.e("captureVideo: ${e.localizedMessage}")
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            REQUEST_ID_READ_WRITE_PERMISSION -> {
+                if (grantResults.size > 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(requireContext(), "Permission granted!", Toast.LENGTH_LONG).show()
+                    recordLowerVideo()
+                } else {
+                    Toast.makeText(requireContext(), "Permission denied!", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+//
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+       // super.onActivityResult(requestCode, resultCode, data)
+
+    Toast.makeText(
+        requireContext(), "Action Cancelled.",
+        Toast.LENGTH_LONG
+    ).show()
+
+        if (requestCode == REQUEST_ID_VIDEO_CAPTURE) {
+            if (resultCode == AppCompatActivity.RESULT_OK) {
+//                val videoUri = data!!.data
+                if (recordVideoResumeViewModel.onVideoDoneEvent.value == true) {
+                    videoFile = _videoFile_for_lower
+                    recordVideoResumeViewModel.videoResumeManagerData.value?.file = _videoFile_for_lower
+                    recordVideoResumeViewModel.uploadSingleVideoToServer(recordVideoResumeViewModel.videoResumeManagerData.value)
+                    showSnackbar()
+                }
+
+            } else if (resultCode == AppCompatActivity.RESULT_CANCELED) {
+                Toast.makeText(
+                    requireContext(), "Action Cancelled.",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(
+                    requireContext(), "Action Failed",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+
+
+
 }
